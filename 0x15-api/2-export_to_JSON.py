7b187cff7493b8f2073b1ad8b3712c5385336630
Felix Adeh Essienne <nanaitconsult@gmail.com>
2023-01-16 19:29:20 +0000

    export_JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+
 """Exports to-do list information for a given employee ID to JSON format."""
+
 import json
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
+if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    tasks = []
+    for task in todo:
+        task_dict = {}
+        task_dict["task"] = task.get("title")
+        task_dict["completed"] = task.get("completed")
+        task_dict["username"] = user.get("username")
+        tasks.append(task_dict)
+    json_obt = {}
+    json_obt[argv[1]] = tasks
+    with open(argv[1] + ".json", "w") as json_file:
+        json.dump(json_obt, json_file)
